617de432be1f80461d0a1ab432bb305f762f43b1
David <david14.dapmi@gmail.com>
2024-04-11 20:20:46 -0600

    prueba operacion finalizada

--- ejercicio2.py
+++ ejercicio2.py
@@ -2,3 +2,6 @@ def suma(num1, num2):
     return num1 + num2
 num1 = float(input("Ingresa el primer numero"))
 num2 = float(input("Ingresa el segundo numero"))
+
+resultado = suma(num1,num2)
+print("El resultado de la suma es:", resultado)
